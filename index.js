0c729d967c63dbf3bfe4353ec29c6abe9c166019
Sarada Prasanna Dash <dashsharada@gmail.com>
2024-12-22 21:53:46 +0530

    notification changes

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const upload = multer({
 app.get("/", (req, res) => {
   return res.render("homepage", {
     deleteMessage:null,
+    downloadMessage:null,
     message: null,
     file: null,
   });
@@ -67,6 +68,7 @@ app.post("/upload", (req, res, next) => {
           console.error("File upload error:", err.message);
           return res.status(400).render("homepage", {
             message: { type: "error", text: err.message },
+            downloadMessage:null,
             deleteMessage:null,
             file: null,
           });
@@ -76,6 +78,7 @@ app.post("/upload", (req, res, next) => {
       if (!req.file) {
         return res.status(400).render("homepage", {
           message: { type: "error", text: "No file uploaded." },
+          downloadMessage:null,
           deleteMessage:null,
           file: null,
         });
@@ -96,6 +99,7 @@ app.post("/upload", (req, res, next) => {
         .then(() => {
           res.status(201).render("homepage", {
             message: { type: "success", text: "File uploaded successfully!" },
+            downloadMessage:null,
             deleteMessage:null,
             file: fileMetadata,
           });
@@ -104,6 +108,7 @@ app.post("/upload", (req, res, next) => {
           console.error("Error saving file metadata:", saveError.message);
           res.status(500).render("homepage", {
             message: { type: "error", text: "Failed to save file metadata." },
+            downloadMessage:null,
             deleteMessage:null,
             file: null,
           });
@@ -118,7 +123,13 @@ app.post("/upload", (req, res, next) => {
       const fileMetadata = await FileMetadata.findOne({ originalname });
   
       if (!fileMetadata) {
-        return res.status(404).json({ message: "File not found" });
+        // return res.status(404).json({ message: "File not found" });
+        return res.status(404).render("homepage", {
+            message: null,
+            deleteMessage:null,
+            downloadMessage: { type: "error", text: "File not found" },
+            file: null,
+          })
       }
   
       
@@ -126,7 +137,12 @@ app.post("/upload", (req, res, next) => {
   
       
       if (!fs.existsSync(filePath)) {
-        return res.status(404).json({ message: "File not found on the server" });
+        return res.status(404).render("homepage", {
+            message: null,
+            deleteMessage:null,
+            downloadMessage: { type: "error", text: "File not found on the server" },
+            file: null,
+          })
       }
   
       res.setHeader("Content-Disposition", `attachment; filename=${fileMetadata.originalname}`);
@@ -155,7 +171,12 @@ app.post("/upload", (req, res, next) => {
       const fileMetadata = await FileMetadata.findOne({ originalname });
   
       if (!fileMetadata) {
-        return res.status(404).json({ message: "File not found in database" });
+        return res.status(404).render("homepage", {
+            message: null,
+            deleteMessage: { type: "error", text: "File not found in database" },
+            downloadMessage: null,
+            file: null,
+          })
       }
   
       const filePath = path.join(__dirname, "uploads",fileMetadata.filename );
@@ -165,6 +186,7 @@ app.post("/upload", (req, res, next) => {
         return res.status(404).render("homepage", {
             message: null,
             deleteMessage: { type: "error", text: "File not found on the server." },
+            downloadMessage: null,
             file: null,
           })
       }
@@ -178,6 +200,7 @@ app.post("/upload", (req, res, next) => {
       res.status(200).render("homepage", {
         message:null,
         deleteMessage: { type: "success", text: "File deleted successfully." },
+        downloadMessage: null,
         file: null,
       });
     } catch (error) {
